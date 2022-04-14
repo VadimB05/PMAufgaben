c839c64850462794e00a4ccd29aa55633747545a
avaysert <a.vajsert12@yahoo.de>
2022-04-14 17:45:12 +0200

    Main prints changed to logging and logger implemented.

--- vorgaben/logging/src/Main.java
+++ vorgaben/logging/src/Main.java
@@ -1,22 +1,38 @@
+import java.io.IOException;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Main {
+
     public static void main(String[] args) {
+        Logger logger = Logger.getLogger(Main.class.getName());
+        ConsoleHandler handlerMain = new ConsoleHandler();
+        handlerMain.setLevel(Level.INFO);
+        handlerMain.setFormatter(new FirstFormatter("MainLogger"));
+        logger.setLevel(Level.INFO);
+        logger.addHandler(handlerMain);
+        logger.setUseParentHandlers(false);
+
+
         Ringbuffer buffer = new Ringbuffer(5);
         try {
             buffer.remove();
         } catch (IllegalStateException stateException) {
-            System.err.println(stateException.getMessage());
+            logger.warning(stateException.getMessage());
         }
         buffer.add("First");
-        System.out.println("First element added succesfully");
+        logger.info("First element added succesfully");
         buffer.add("Second");
-        System.out.println("Second element added Succesfully");
+        logger.info("Second element added Succesfully");
         try {
             buffer.add("Third");
-            System.out.println("Third element added Succesfully");
+            logger.info("Third element added Succesfully");
         } catch (IllegalStateException stateException) {
-            System.err.println(stateException.getMessage());
+            logger.warning(stateException.getMessage());
         }
         String first = buffer.remove();
-        System.out.printf("The removed element is: %s", first);
+        logger.info("The removed element is: "+ first);
     }
 }
