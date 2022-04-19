d7cc27b64f38ea888e238cdbeb46b0e94ccc4d7e
avaysert <a.vajsert12@yahoo.de>
2022-04-19 18:34:26 +0200

    animations for running left and right, automatically switching through idle and running animations

--- dungeon/code/desktop/src/desktop/MyHero.java
+++ dungeon/code/desktop/src/desktop/MyHero.java
@@ -13,16 +13,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MyHero extends Animatable {
-    private Animation idleAnimation;
+    private Animation animation, runAnimationRight,runAnimationLeft,idleAnimationRight, idleAnimationLeft;
     private Point position;
     private Level currentLevel;
+    private boolean isLookingLeft = false;
 
     public MyHero(Painter painter, SpriteBatch batch){
         super(painter, batch);
-        List<String> animation = new ArrayList<>();
-        animation.add("character/knight/knight_m_idle_anim_f0.png");
-        animation.add("character/knight/knight_m_idle_anim_f1.png");
-        idleAnimation = new Animation(animation,8);
+        List<String> idleAnimationRightList = new ArrayList<>();
+        List<String> idleAnimationLeftList = new ArrayList<>();
+        List<String> runAnimationRightList = new ArrayList<>();
+        List<String> runAnimationLeftList = new ArrayList<>();
+        idleAnimationRightList.add("character/knight/knight_m_idle_anim_f0.png");
+        idleAnimationRightList.add("character/knight/knight_m_idle_anim_f1.png");
+        idleAnimationRightList.add("character/knight/knight_m_idle_anim_f2.png");
+        idleAnimationRightList.add("character/knight/knight_m_idle_anim_f3.png");
+        idleAnimationRight = new Animation(idleAnimationRightList,8);
+        idleAnimationLeftList.add("character/knight/knight_m_idle_anim_mirrored_f0.png");
+        idleAnimationLeftList.add("character/knight/knight_m_idle_anim_mirrored_f1.png");
+        idleAnimationLeftList.add("character/knight/knight_m_idle_anim_mirrored_f2.png");
+        idleAnimationLeftList.add("character/knight/knight_m_idle_anim_mirrored_f3.png");
+        idleAnimationLeft = new Animation(idleAnimationLeftList,8);
+        runAnimationRightList.add("character/knight/knight_m_run_anim_f0.png");
+        runAnimationRightList.add("character/knight/knight_m_run_anim_f1.png");
+        runAnimationRightList.add("character/knight/knight_m_run_anim_f2.png");
+        runAnimationRightList.add("character/knight/knight_m_run_anim_f3.png");
+        runAnimationRight = new Animation(runAnimationRightList,8);
+        runAnimationLeftList.add("character/knight/knight_m_run_anim_mirrored_f0.png");
+        runAnimationLeftList.add("character/knight/knight_m_run_anim_mirrored_f1.png");
+        runAnimationLeftList.add("character/knight/knight_m_run_anim_mirrored_f2.png");
+        runAnimationLeftList.add("character/knight/knight_m_run_anim_mirrored_f3.png");
+        runAnimationLeft = new Animation(runAnimationLeftList,8);
+        animation = idleAnimationRight;
     }
 
 
@@ -36,18 +58,32 @@ public class MyHero extends Animatable {
     public void update() {
         Point newPosition = new Point(this.position);
         float movementSpeed = 0.1f;
-
+        
         if(Gdx.input.isKeyPressed(Input.Keys.W)){
             newPosition.y += movementSpeed;
+            isRunningLeft();
         }
-        if(Gdx.input.isKeyPressed(Input.Keys.S)){
+        else if(Gdx.input.isKeyPressed(Input.Keys.S)){
             newPosition.y -= movementSpeed;
+            isRunningLeft();
         }
-        if(Gdx.input.isKeyPressed(Input.Keys.D)){
+        else if(Gdx.input.isKeyPressed(Input.Keys.D)){
+            isLookingLeft = false;
             newPosition.x += movementSpeed;
+            animation = runAnimationRight;
         }
-        if(Gdx.input.isKeyPressed(Input.Keys.A)){
+        else if(Gdx.input.isKeyPressed(Input.Keys.A)){
+            isLookingLeft = true;
             newPosition.x -= movementSpeed;
+            animation = runAnimationLeft;
+        }
+        else {
+            if(isLookingLeft){
+                animation = idleAnimationLeft;
+            }
+            else {
+                animation = idleAnimationRight;
+            }
         }
 
         if(currentLevel.getTileAt(newPosition.toCoordinate()).isAccessible()){
@@ -62,6 +98,15 @@ public class MyHero extends Animatable {
 
     @Override
     public Animation getActiveAnimation() {
-        return idleAnimation;
+        return animation;
+    }
+
+    public void isRunningLeft(){
+        if(isLookingLeft){
+            animation = runAnimationLeft;
+        }
+        else{
+            animation = runAnimationRight;
+        }
     }
 }
