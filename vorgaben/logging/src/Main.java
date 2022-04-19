7c5c957eb92b3b83986d5edff435f4af05bd86bf
JamalDerIV <93540563+JamalDerIV@users.noreply.github.com>
2022-04-19 17:25:10 +0200

    Revert "Develope"

--- vorgaben/logging/src/Main.java
+++ vorgaben/logging/src/Main.java
@@ -1,48 +1,22 @@
-import java.io.IOException;
-import java.util.logging.ConsoleHandler;
-import java.util.logging.FileHandler;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 public class Main {
-
     public static void main(String[] args) {
-        Logger logger = Logger.getLogger(Main.class.getName());
-        ConsoleHandler handlerMain = new ConsoleHandler();
-        FileHandler errorData;
-        handlerMain.setLevel(Level.INFO);
-        handlerMain.setFormatter(new FirstFormatter("MainLogger"));
-        logger.setLevel(Level.INFO);
-        logger.addHandler(handlerMain);
-        logger.setUseParentHandlers(false);
-
-        try {
-            errorData = new FileHandler("data/errorDataMain.csv",true);
-            errorData.setFormatter(new SecondFormatter("MainLogger"));
-            errorData.setLevel(Level.WARNING);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        logger.addHandler(errorData);
-
         Ringbuffer buffer = new Ringbuffer(5);
         try {
             buffer.remove();
         } catch (IllegalStateException stateException) {
-            logger.warning(stateException.getMessage());
+            System.err.println(stateException.getMessage());
         }
         buffer.add("First");
-        logger.info("First element added succesfully");
+        System.out.println("First element added succesfully");
         buffer.add("Second");
-        logger.info("Second element added Succesfully");
+        System.out.println("Second element added Succesfully");
         try {
             buffer.add("Third");
-            logger.info("Third element added Succesfully");
+            System.out.println("Third element added Succesfully");
         } catch (IllegalStateException stateException) {
-            logger.warning(stateException.getMessage());
+            System.err.println(stateException.getMessage());
         }
         String first = buffer.remove();
-        logger.info("The removed element is: "+ first);
+        System.out.printf("The removed element is: %s", first);
     }
 }
