32a6fa993906eab918cfb2d1fa124d2d124d8ede
avaysert <a.vajsert12@yahoo.de>
2022-04-28 10:59:23 +0200

    hud package und font package hinzugefuegt, eine Schriftart für die Levelanzeige ausgewaehlt und das HUD implementiert

--- dungeon/code/desktop/src/desktop/MyGame.java
+++ dungeon/code/desktop/src/desktop/MyGame.java
@@ -1,13 +1,18 @@
 package desktop;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import controller.MainController;
+import hud.Icon;
 import level.generator.LevelLoader.LevelLoader;
 import level.generator.dungeong.graphg.NoSolutionException;
+import tools.Point;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
 
 public class MyGame extends MainController {
-
+    private Label levelLabel;
     private MyHero hero;
+    private int levelCounter = 0;
 
 
 
@@ -25,6 +30,7 @@ public class MyGame extends MainController {
         }
         camera.follow(hero);
         entityController.add(hero);
+        hudController.add(new Icon(hudPainter,hudBatch,new Point(0f,0f)));
     }
 
     @Override
@@ -43,6 +49,13 @@ public class MyGame extends MainController {
 
     @Override
     public void onLevelLoad() {
+        levelCounter++;
+        if(levelCounter==1){
+            levelLabel = hudController.drawText("Level "+levelCounter,"font/PublicPixel-0W5Kv.ttf", Color.YELLOW,30,50,50,30,30);
+        }
+        else{
+            levelLabel.setText("Level "+levelCounter);
+        }
         hero.setLevel(levelAPI.getCurrentLevel());
     }
 
---
+++ dungeon/code/desktop/src/hud/Icon.java
@@ -0,0 +1,34 @@
+package hud;
+
+import basiselements.HUDElement;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import graphic.HUDPainter;
+import tools.Point;
+
+public class Icon extends HUDElement {
+    private String texturePath;
+    private Point position;
+
+    /**
+     * A object that can be controlled by the <code>HUDController
+     * </code>.
+     *
+     * @param painter Painter that draws this object
+     * @param batch   Batch to draw on
+     */
+    public Icon(HUDPainter painter, SpriteBatch batch, Point position) {
+        super(painter, batch);
+        texturePath = "hud/ui_heart_full.png";
+        this.position = position;
+    }
+
+    @Override
+    public Point getPosition() {
+        return position;
+    }
+
+    @Override
+    public String getTexturePath() {
+        return texturePath;
+    }
+}
